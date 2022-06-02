b649fb045a2f2eaee8f335e0aa85a1edb2138f3b
baku_nishiya <planet.of.early.summer.rain@gmail.com>
2022-06-02 22:17:44 +0900

    6章まで実装完了

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,71 +1,53 @@
-// --- ここから追加 ---
-import React, { Fragment, useEffect, useReducer } from 'react';
-import styled from 'styled-components';
+import React, { Fragment, useEffect, useReducer } from 'react'
+import styled from 'styled-components'
 import { Link } from "react-router-dom"
-
-import Skeleton from '@material-ui/lab/Skeleton';
-
-// apis
-import { fetchRestaurants } from '../apis/restaurants';
-
-// reducers
+import Skeleton from '@material-ui/lab/Skeleton'
+import { fetchRestaurants } from '../apis/restaurants'
 import {
   initialState,
   restaurantsActionTypes,
   restaurantsReducer,
-} from '../reducers/restaurants';
-
-// constants
-import { REQUEST_STATE } from '../constants';
-
-// images
-import MainLogo from '../images/logo.png';
-import MainCoverImage from '../images/main-cover-image.png';
-import RestaurantImage from '../images/restaurant-image.jpg';
+} from '../reducers/restaurants'
+import { REQUEST_STATE } from '../constants'
+import MainLogo from '../images/logo.png'
+import MainCoverImage from '../images/main-cover-image.png'
+import RestaurantImage from '../images/restaurant-image.jpg'
 
 const HeaderWrapper = styled.div`
   display: flex;
   justify-content: flex-start;
   padding: 8px 32px;
-`;
-
+`
 const MainLogoImage = styled.img`
   height: 90px;
 `
-
 const MainCoverImageWrapper = styled.div`
   text-align: center;
-`;
-
+`
 const MainCover = styled.img`
   height: 600px;
-`;
-
+`
 const RestaurantsContentsList = styled.div`
   display: flex;
   justify-content: space-around;
   margin-bottom: 150px;
-`;
-
+`
 const RestaurantsContentWrapper = styled.div`
   width: 450px;
   height: 300px;
   padding: 48px;
-`;
-
+`
 const RestaurantsImageNode = styled.img`
   width: 100%;
-`;
-
+`
 const MainText = styled.p`
   color: black;
   font-size: 18px;
-`;
-
+`
 const SubText = styled.p`
   color: black;
   font-size: 12px;
-`;
+`
 
 export const Restaurants = () => {
   const [state, dispatch] = useReducer(restaurantsReducer, initialState)
